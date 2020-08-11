b15e8aef8c709c0dcde0317e2a2827286aad078f
JeremiahUy <jery_3000@hotmail.com>
2020-08-11 12:11:58 +0200

    UPDATE: to use hooks components

--- packages/viewers/table/src/pages/Viewer.tsx
+++ packages/viewers/table/src/pages/Viewer.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import HandleAxiosError from '../utils/HandleAxiosError';
 import env from '@beam-australia/react-env';
-import axios from 'axios';
-import {Spinner} from 'baseui/spinner';
 import {Block} from 'baseui/block';
-import Cookies from 'js-cookie';
-import {Header} from '@dakan/ui';
+import {Header, LoadingSpinner} from '@dakan/ui';
 import {Metrics} from '@dakan/metrics';
+import { useNode, useNodeEdges, useContent } from '@dakan/hooks'
 
 import Content from '../component/Content';
 import exampleJson from '../resources/example.json';
@@ -15,122 +12,52 @@ import exampleTags from '../resources/exampleTags.json';
 import exampleComments from '../resources/exampleComments.json';
 import TableNotFound from '../component/TableNotFound';
 
-const graph_server = env('GRAPH_SERVER');
 const title = env('TITLE') || 'Data';
-const link = env('HOME_URL') || '../';
 
 const Viewer = (props: any) => {
-    const [data, setData] = React.useState({});
-    const [columns, setColumns] = React.useState([{}]);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [error, setError] = React.useState(null);
-    const [tagOptions, setTagOptions] = React.useState([{}]);
-    const [numberOfColumns, setNumberOfColumns] = React.useState(0);
-    const [showSpinner, setShowSpinner] = React.useState(true);
-    const [clientUser, setClientUser] = React.useState({});
-    const [comments, setComments] = React.useState([{}]);
-    const [page, setPage] = React.useState('');
 
-    const {id} = props.match.params;
+    const [node, loadingNode, errorLoadingNode] = useNode(props.match.params.id)
+    const [columns, loadingColumns, errorLoadingColumns] = useNodeEdges(props.match.params.id, 'hasMember')
+    const [tagOptions, loadingtagOptions, errorLoadingtagOptions] = useContent('opplysningstype')
+    const [comments, loadingCommnets, errorLoadingComments, setComments] = useNodeEdges(props.match.params.id, 'hasComment')
 
-    const handleGetEdgesResponse = (response: any) => {
-        if (typeof response.data === 'object' && response.data !== null) {
-            setColumns(response.data);
-            setNumberOfColumns(response.data.length);
-            setShowSpinner(false);
-        } else {
-            setError(response);
-            setShowSpinner(false);
-        }
-    };
+    const getHeader = () => (
+        <Header
+          config={{
+            nav: true,
+            about: true,
+            showLoginButton: true
+          }}
+        />
+      )
+    
+      if (props.match.params.id === 'test') {
+        return (
+          <Block>
+            {getHeader()}
+            <Content
+              {...props}
+              data={exampleJson}
+              columns={exampleColumnJson}
+              tagOptions={exampleTags}
+              numberOfColumns={exampleColumnJson.length}
+              comments={exampleComments}
+              setComments={setComments}
+            />
+          </Block>
+        )
+      }
 
-    const handleGetCommentsResponse = (response: any) => {
-        if (typeof response.data === 'object' && response.data !== null) {
-            const commentList = [...response.data].sort((a, b) => {
-                if (a.properties.date + 't' + a.properties.time < b.properties.date + 't' + b.properties.time) {
-                    return 1;
-                }
-                if (a.properties.date + 't' + a.properties.time > b.properties.date + 't' + b.properties.time) {
-                    return -1;
-                }
-                return 0;
-            });
-            setComments(commentList);
-        } else {
-            setError(response);
-        }
-    };
-
-    const handleGetIndexResponse = (response: any) => {
-        if (typeof response === 'object' && response !== null) {
-            setData(response);
-            if (response.id) {
-                axios
-                    .get(`${graph_server}/node/out/${id}/hasMember`)
-                    .then(handleGetEdgesResponse)
-                    .catch((e) => HandleAxiosError(e, setError));
-                axios
-                    .get(`${graph_server}/node/out/${id}/hasComment`)
-                    .then(handleGetCommentsResponse)
-                    .catch((e) => HandleAxiosError(e, setError));
-            }
-            setPage(response.properties.table_name);
-        } else {
-            setError(response);
-        }
-    };
-
-    const handleGetInformationTypeResponse = async (response: any) => {
-        if (typeof response === 'object' && response !== null) {
-            setTagOptions(response);
-        } else {
-            setError(response);
-        }
-    };
-
-    const getAzureAuth = () => {
-        const tokenId = Cookies.get('ClientToken');
-        const clientUser = Cookies.get('ClientUser');
-        if (tokenId && clientUser) {
-            setClientUser(JSON.parse(clientUser.replace(/\\054/g, ',').replace(/\\"/g, '"')));
-        }
-    };
-
-    React.useEffect(() => {
-        if (id === 'test' || id === 'test.test') {
-            setData(exampleJson);
-            setComments(exampleComments);
-            setColumns(exampleColumnJson);
-            setTagOptions(exampleTags);
-            setNumberOfColumns(exampleColumnJson.length);
-            setShowSpinner(false);
-            getAzureAuth();
-        } else {
-            axios
-                .get(`${graph_server}/node/${id}`)
-                .then((response: any) => handleGetIndexResponse(response.data))
-                .catch((e) => {
-                    HandleAxiosError(e, setError);
-                    setShowSpinner(false);
-                });
-            axios
-                .get(`${graph_server}/nodes/opplysningstype`)
-                .then((response: any) => handleGetInformationTypeResponse(response.data))
-                .catch((e) => HandleAxiosError(e, setError));
-            getAzureAuth();
-        }
-    }, [id]);
-
-    if (showSpinner) {
+    if (loadingNode) {
         return (
             <Block display="flex" justifyContent="center">
-                <Spinner size={96} />
+                <LoadingSpinner size={96} />
             </Block>
         );
     }
 
-    if (error && !Object.keys(data).length) {
-        return <TableNotFound error={error} />;
+    if (errorLoadingNode && !Object.keys(node).length) {
+        return <TableNotFound error={errorLoadingNode} />;
     }
 
     return (
@@ -140,31 +67,26 @@ const Viewer = (props: any) => {
                     brand: title,
                     nav: true,
                     about: true,
-                    aboutLink: 'https://data.nav.no/about',
-                    link: link,
-                    showLoginButton: true,
-                    clientUser: clientUser,
-                    server: graph_server,
+                    showLoginButton: true
                 }}
             />
-            {data && Object.keys(data).length && (
+            {node && Object.keys(node).length && (
                 <Metrics
                     viewer={'tabell'}
-                    page={page}
+                    page={node.id}
                     section={''}
                 />
             )}
-            {data && (
+            {node && (
                 <Block>
                     <Block display="flex" justifyContent="flex-end"></Block>
                     <Content
-                        data={data}
+                        data={node}
                         columns={columns}
                         tagOptions={tagOptions}
                         comments={comments}
                         setComments={setComments}
-                        numberOfColumns={numberOfColumns}
-                        clientUser={clientUser}
+                        numberOfColumns={columns.length}
                     />
                 </Block>
             )}
