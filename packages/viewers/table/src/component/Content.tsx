ef194851644fc1e5c9916d0a7cfd541429709b5b
JeremiahUy <jery_3000@hotmail.com>
2020-08-11 12:11:46 +0200

    UPDATE: ui package prop usage

--- packages/viewers/table/src/component/Content.tsx
+++ packages/viewers/table/src/component/Content.tsx
@@ -88,7 +88,6 @@ const Content = (props: any): JSX.Element => {
                             dataId={props.data.id}
                             comments={props.comments}
                             setComments={props.setComments}
-                            clientUser={props.clientUser}
                             server={server}
                         />
                         {props.columns && (
@@ -107,7 +106,6 @@ const Content = (props: any): JSX.Element => {
                                 <TableColumns
                                     columnsToView={filterText ? filteredColumns : props.columns}
                                     tagOptions={props.tagOptions}
-                                    clientUser={props.clientUser}
                                 />
                             </React.Fragment>
                         )}
