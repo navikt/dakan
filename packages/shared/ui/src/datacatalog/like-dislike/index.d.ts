2ffeafa1875ff8e0a90f145c377d221ca0050f91
JeremiahUy <jery_3000@hotmail.com>
2020-08-13 13:39:12 +0200

    ADD: like-dislike rating component

---
+++ packages/shared/ui/src/datacatalog/like-dislike/index.d.ts
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export interface CustomLike_DislikeRatingProps {
+  theme?: any
+  spec?: any
+}
+
+export declare const Rating: React.FC<CustomLike_DislikeRatingProps>
+export default Rating
