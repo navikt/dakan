a77acc6bedd7e65ec0a2b0329747a74982de71ea
JeremiahUy <jery_3000@hotmail.com>
2020-08-17 13:08:37 +0200

    UPDATE: component to be able to be reused in other applications

---
+++ packages/shared/ui/src/datacatalog/tagging/index.d.ts
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+export interface TaggingProps {
+  tagOptions?: any
+  dataId?: any
+  serverUrl?: any
+  dataTags?: any
+  setdataTags?: any
+  tokenId?: any
+  clientUser?: any
+  edgeLabel: string
+  tagLabel?: string
+}
+
+export declare const SelectOpplysningstype: React.FC<TaggingProps>
+export default SelectOpplysningstype
