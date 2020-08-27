2745004419efd932193bd6fee8cfd5a0e736e67d
JeremiahUy <jery_3000@hotmail.com>
2020-08-27 12:00:35 +0200

    ADD: filter

--- packages/viewers/table/src/component/SelectPerson.tsx
+++ packages/viewers/table/src/component/SelectPerson.tsx
@@ -37,6 +37,11 @@ const buildAutocompleteRequest = (searchTerm: any) => {
             },
           },
         ],
+        filter: {
+          term: {
+            type : 'person'
+          }
+        }
       },
     },
   }
