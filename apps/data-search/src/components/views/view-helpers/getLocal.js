b8a01a8391a1d60de2fdb5709e1e3298f41fb1ef
JeremiahUy <jery_3000@hotmail.com>
2020-09-02 14:07:05 +0200

    UPDATE: get local method to return new labels

--- apps/data-search/src/components/views/view-helpers/getLocal.js
+++ apps/data-search/src/components/views/view-helpers/getLocal.js
@@ -5,10 +5,17 @@ export function getLocal(label) {
   if (label === 'godkjent_begrep') return 'Godkjent begrep'
   if (label === 'purpose') return 'Formål'
   if (label === 'pii-type') return 'Opplysningstype'
+  if (label === 'opplysningstype') return 'Opplysningstype'
   if (label === 'kafka') return 'Datastrøm'
   if (label === 'tabell') return 'Tabell'
   if (label === 'kafka topic') return 'Kafka'
   if (label === 'tableau') return 'Tableau'
+  if (label === 'person') return 'Person'
+  if (label === 'NaisApp') return 'Nais App'
+  if (label === 'NaisTeam') return 'Nais Team'
+  if (label === 'office') return 'Kontor'
+  if (label === 'ProductArea') return 'Produktområde'
+  if (label === 'team') return 'Team'
 
   return label
 }
