3db60903612981c260e76755f52e1cd940765703
andre groseth <andregroseth@hotmail.com>
2020-10-29 10:47:32 +0100

    UPDATE: Capitalize String

--- packages/shared/ui/src/utils/CapitalizeString/CapitalizeString.js
+++ packages/shared/ui/src/utils/CapitalizeString/CapitalizeString.js
@@ -1,4 +1,4 @@
 export const CapitalizeString = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1)
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
 }
 export default CapitalizeString
