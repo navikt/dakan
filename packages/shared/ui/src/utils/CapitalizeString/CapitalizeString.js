21026aeb69b6b824b5282f01458948872807e9e5
andre groseth <andregroseth@hotmail.com>
2020-10-29 11:26:18 +0100

    ADD: Check to capitalizeString

--- packages/shared/ui/src/utils/CapitalizeString/CapitalizeString.js
+++ packages/shared/ui/src/utils/CapitalizeString/CapitalizeString.js
@@ -1,4 +1,8 @@
-export const CapitalizeString = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+export const CapitalizeString = (word) => {
+  if (word instanceof String || typeof word === 'string'){
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  } else {
+    return word
+  }
 }
 export default CapitalizeString
