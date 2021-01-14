df8b89403e049223b8079c94ca8e767e6472d183
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-01-14 13:47:00 +0100

    UPDATE: to handle markdown text
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: erikvatt <erik.vattekar@nav.no>

--- packages/viewers/api/src/components/Content.tsx
+++ packages/viewers/api/src/components/Content.tsx
@@ -3,6 +3,8 @@ import { Block } from 'baseui/block'
 import { ParagraphMedium } from 'baseui/typography'
 import { LayoutSplit as Layout, ContentItems } from '@dakan/ui'
 import { renderDeclarativeRules } from 'styletron-standard'
+import ReactMarkdown from 'react-markdown'
+import { useStyletron } from 'baseui'
 
 const ITEMS = [
   { item: 'slack', label: 'Slack', format: 'slackchannel' },
@@ -10,18 +12,31 @@ const ITEMS = [
 ]
 
 const Content = ({ item, id }) => {
-  const Head = () =>{ 
+  const [, theme] = useStyletron()
+  const Head = () => {
     const itemProperties = {
       properties: item.content
     }
     return (
-    <Block>
-      <ParagraphMedium>
-        {item.content.description }
-      </ParagraphMedium>
-      <ContentItems ITEMS={ITEMS} item={itemProperties} />
-    </Block>
-  )}
+      <Block
+        overrides={{
+          Block: {
+            style: {
+              marginTop: 0,
+              marginBottom: 0,
+              overflowWrap: 'break-word',
+              wordWrap: 'break-word',
+              wordBreak: 'break-word',
+              ...theme.typography.font300,
+            },
+          },
+        }}
+      >
+        <ReactMarkdown source={item.content.description} />
+        <ContentItems ITEMS={ITEMS} item={itemProperties} />
+      </Block>
+    )
+  }
 
   const getHeadingText = () => {
     return <Block>{item.content.name}</Block>
@@ -30,14 +45,14 @@ const Content = ({ item, id }) => {
   return (
     <div role="main">
       {item && item.content && (
-      <Block>
-        <Layout
-          headingLabel="API"
-          headingText={getHeadingText()}
-          left={<Head />}
-          right={null}
-        />
-      </Block>)}
+        <Block>
+          <Layout
+            headingLabel="API"
+            headingText={getHeadingText()}
+            left={<Head />}
+            right={null}
+          />
+        </Block>)}
     </div>
   )
 }
