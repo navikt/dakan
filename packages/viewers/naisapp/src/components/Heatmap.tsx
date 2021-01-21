c826e6083f2243673414afd3bc6129e7a0a1b161
andre groseth <andregroseth@hotmail.com>
2021-01-21 16:34:30 +0100

    ADD: New heatmap

--- packages/viewers/naisapp/src/components/Heatmap.tsx
+++ packages/viewers/naisapp/src/components/Heatmap.tsx
@@ -3,16 +3,52 @@ import CalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
 import ReactTooltip from 'react-tooltip';
 
+import { ResponsiveCalendar } from '@nivo/calendar'
+import { Block } from 'baseui/block';
+
 const Heatmap = (props) => {
   const { data } = props
-  
+
   const today = new Date().toISOString().split('T')[0]
   const oneYearAgo = new Date()
-  oneYearAgo.setFullYear(oneYearAgo.getFullYear()-1)
+  oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1)
+
+  const nivoTheme = {
+    "background": "#ffffff",
+    "textColor": "black",
+    "fontSize": 16,
+  }
 
   return (
-    <React.Fragment>
-      <CalendarHeatmap
+      <Block aria-label="Github heatmap historikk" height="240px" width="100%">
+        <ResponsiveCalendar
+          data={data}
+          theme={nivoTheme}
+          from={oneYearAgo.toISOString().split('T')[0]}
+          to={today}
+          align="top-left"
+          emptyColor="#eeeeee"
+          colors={['#d6e685', '#8cc665', '#44a340;', '#1e6823']}
+          margin={{ top: 40, right: 0, bottom: 0, left: 40 }}
+          yearSpacing={40}
+          monthBorderColor="#ffffff"
+          dayBorderWidth={2}
+          dayBorderColor="#ffffff"
+          legends={[
+            {
+              anchor: 'bottom-right',
+              direction: 'row',
+              translateY: 36,
+              itemCount: 4,
+              itemWidth: 42,
+              itemHeight: 36,
+              itemsSpacing: 14,
+              itemDirection: 'right-to-left'
+            }
+          ]}
+        />
+
+        {/* <CalendarHeatmap
       startDate={oneYearAgo.toISOString().split('T')[0]}
       endDate={today}
         values={data}
@@ -46,9 +82,9 @@ const Heatmap = (props) => {
           }
         }}
         showWeekdayLabels={true}
-      />
-      <ReactTooltip />
-    </React.Fragment>
+      /> */}
+        <ReactTooltip />
+      </Block>
   )
 }
 
