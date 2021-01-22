c97064c03d55c4b1ffaeef789a03432e4849e45c
andre groseth <andregroseth@hotmail.com>
2021-01-22 10:58:53 +0100

    REMOVE: Unused code

--- packages/viewers/naisapp/src/components/Heatmap.tsx
+++ packages/viewers/naisapp/src/components/Heatmap.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import CalendarHeatmap from 'react-calendar-heatmap';
-import 'react-calendar-heatmap/dist/styles.css';
-import ReactTooltip from 'react-tooltip';
 
 import { ResponsiveCalendar } from '@nivo/calendar'
 import { Block } from 'baseui/block';
@@ -47,43 +44,6 @@ const Heatmap = (props) => {
             }
           ]}
         />
-
-        {/* <CalendarHeatmap
-      startDate={oneYearAgo.toISOString().split('T')[0]}
-      endDate={today}
-        values={data}
-        classForValue={value => {
-          if (!value) {
-            return 'color-empty';
-          }
-          else if (value.count <= 10) {
-            return 'color-github-1'
-          }
-          else if (value.count <= 20) {
-            return 'color-github-2'
-          }
-          else if (value.count <= 30) {
-            return 'color-github-3'
-          }
-          else {
-            return 'color-github-4'
-          }
-        }}
-        tooltipDataAttrs={value => {
-          if (!value.count) {
-            return {
-              'data-tip': "0 commits"
-            }
-          }
-          else {
-            return {
-              'data-tip': `${value.date} has ${value.count} commits`,
-            };
-          }
-        }}
-        showWeekdayLabels={true}
-      /> */}
-        <ReactTooltip />
       </Block>
   )
 }
