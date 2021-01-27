20439589d3465c38bdffc41e25b145de09fa054a
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-01-27 15:34:29 +0100

    UPDATE: to show api description as link
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- packages/viewers/api/src/components/Content.tsx
+++ packages/viewers/api/src/components/Content.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Block } from 'baseui/block'
 import { ParagraphMedium } from 'baseui/typography'
+import { StyledLink } from 'baseui/link'
 import { LayoutSplit as Layout, ContentItems, LabeledContent } from '@dakan/ui'
 import { renderDeclarativeRules } from 'styletron-standard'
 import ReactMarkdown from 'react-markdown'
@@ -8,14 +9,14 @@ import { useStyletron } from 'baseui'
 
 const ITEMS = [
   { item: 'apiRevision', label: 'Api Revision' },
-  { item: 'subscriptionRequired', label: 'Subscription Required'},
-  { item: 'serviceUrl', label: 'Service Url'},
-  { item: 'path', label: 'Path'},
-  { item: 'protocols', label: 'Protocols'},
-  { item: 'authenticationSettings', label: 'Authentication Settings'},
-  { item: 'subscriptionKeyParameterNames', label: 'Subscription Key Parameter Names'},
-  { item: 'isCurrent', label: 'Is Current'},
-  { item: 'apiVersion', label: 'Api Version'},
+  { item: 'subscriptionRequired', label: 'Subscription Required' },
+  { item: 'serviceUrl', label: 'Service Url' },
+  { item: 'path', label: 'Path' },
+  { item: 'protocols', label: 'Protocols' },
+  { item: 'authenticationSettings', label: 'Authentication Settings' },
+  { item: 'subscriptionKeyParameterNames', label: 'Subscription Key Parameter Names' },
+  { item: 'isCurrent', label: 'Is Current' },
+  { item: 'apiVersion', label: 'Api Version' },
 ]
 
 const Content = ({ item, id }) => {
@@ -23,25 +24,25 @@ const Content = ({ item, id }) => {
   const Head = () => {
     return (
       <div role="main">
-      <Block
-        overrides={{
-          Block: {
-            style: {
-              marginTop: 0,
-              marginBottom: 0,
-              overflowWrap: 'break-word',
-              wordWrap: 'break-word',
-              wordBreak: 'break-word',
-              ...theme.typography.font300,
+        <Block
+          overrides={{
+            Block: {
+              style: {
+                marginTop: 0,
+                marginBottom: 0,
+                overflowWrap: 'break-word',
+                wordWrap: 'break-word',
+                wordBreak: 'break-word',
+                ...theme.typography.font300,
+              },
             },
-          },
-        }}
-      >
-        <ReactMarkdown source={item.content.description} />
-        <LabeledContent description="Api description" list>
-          {item.content.api_dec}
-        </LabeledContent>
-      </Block>
+          }}
+        >
+          <ReactMarkdown source={item.content.description} />
+          <LabeledContent description="Api description" list>
+            <StyledLink aria-label="Link til Api beskrivelse" href={item.content.api_dec}>{item.content.api_dec}</StyledLink>
+          </LabeledContent>
+        </Block>
       </div>
     )
   }
@@ -50,9 +51,6 @@ const Content = ({ item, id }) => {
     const itemProperties = {
       properties: item.content
     }
-
-    const test = true
-  
     return (
       <div role="complementary" >
         <ContentItems ITEMS={ITEMS} item={itemProperties} />
