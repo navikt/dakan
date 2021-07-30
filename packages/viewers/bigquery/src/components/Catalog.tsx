5131843587cd40442e1c2610e00d5237cf515e19
Paul Bencze <paul@idelab.com>
2021-07-30 13:20:20 +0200

    add lineage graph

--- packages/viewers/bigquery/src/components/Catalog.tsx
+++ packages/viewers/bigquery/src/components/Catalog.tsx
@@ -1,27 +1,65 @@
 import * as React from 'react'
 import { Block } from 'baseui/block'
+import { Table } from 'baseui/table-semantic'
 import { Label } from '@dakan/ui'
+
 import { useCatalog } from '../hooks/useCatalog'
 
 const Catalog = ({ url, dataset_id }) => {
   const [data, loading, error] = useCatalog(url, dataset_id)
-  const dataset_name = 'model.ereg_dbt.' + dataset_id.split('.')[2]
 
   if (!data) {
     return null
   }
 
-  const node = data['nodes'][dataset_name]
-  const rows = node['stats']
+  const dataset_id_parts = dataset_id.split(/[\s.]+/)
+  const dataset_name = dataset_id_parts[dataset_id_parts.length - 1]
+
+  const node = data['nodes']['model.ereg_dbt.' + dataset_name]
+
+  let num_bytes = ''
+  if (node['stats']['has_stats']['value']) {
+    //num_bytes += node['stats']['num_bytes']['description']
+    //num_bytes += ` (${node['stats']['num_bytes']['label']})`
+    num_bytes += `Størrelse i bytes: ${node['stats']['num_bytes']['value']}`
+  }
+
+  let num_rows = ''
+  if (node['stats']['has_stats']['value']) {
+    //num_rows += node['stats']['num_rows']['description']
+    //num_rows += ` (${node['stats']['num_rows']['label']})`
+    num_rows += `Antall rader: ${node['stats']['num_rows']['value']}`
+  }
+
+  const Schema = () => {
+    if (node['columns']) {
+      const columns = node['columns']
+      if (typeof columns !== 'object') {
+        return null
+      }
+      let content: any[] = []
+      Object.keys(columns).forEach((key) => {
+        const col = columns[key]
+        content.push([col.type, col.name, col.comment])
+      })
+      return <Table columns={['Type', 'Navn', 'Kommentar']} data={content} />
+    }
+    return null
+  }
 
   return (
     <React.Fragment>
       {data && (
         <Block>
-          <Label>Catalog</Label>
-          <Block>{JSON.stringify({ rows })}</Block>
-          <Label>Node</Label>
-          <Block>{JSON.stringify({ node })}</Block>
+          <Label>Stats</Label>
+          <Block marginTop="scale800">{num_rows}</Block>
+          <Block>{num_bytes}</Block>
+          <Block marginTop="scale1200">
+            <Label>Skjema</Label>
+            <Block marginTop="scale800">
+              <Schema />
+            </Block>
+          </Block>
         </Block>
       )}
     </React.Fragment>
--- packages/viewers/bigquery/src/components/Lineage.tsx
+++ packages/viewers/bigquery/src/components/Lineage.tsx
@@ -2,19 +2,111 @@ import * as React from 'react'
 import { Block } from 'baseui/block'
 import { Label } from '@dakan/ui'
 import { useLineage } from '../hooks/useLineage'
-import { string } from 'prop-types'
+import { EChart } from '@deetly/echart-view'
+
+const getOption = (data) => {
+  const option = {
+    tooltip: {
+      trigger: 'item',
+      triggerOn: 'mousemove',
+    },
+    series: [
+      {
+        type: 'tree',
+        id: 0,
+        name: 'tree1',
+        data: [data],
+
+        top: '10%',
+        left: '12%',
+        bottom: '22%',
+        right: '20%',
+
+        symbolSize: 7,
+
+        edgeShape: 'polyline',
+        edgeForkPosition: '63%',
+        initialTreeDepth: 1,
+
+        lineStyle: {
+          width: 2,
+        },
+
+        label: {
+          backgroundColor: '#fff',
+          position: 'left',
+          verticalAlign: 'middle',
+          align: 'right',
+        },
+
+        leaves: {
+          label: {
+            position: 'right',
+            verticalAlign: 'middle',
+            align: 'left',
+          },
+        },
+
+        emphasis: {
+          focus: 'descendant',
+        },
+
+        expandAndCollapse: true,
+        animationDuration: 550,
+        animationDurationUpdate: 750,
+      },
+    ],
+  }
+  return option
+}
+
+const getLast = (input: string) => {
+  const el = input.split(/[\s.]+/)
+  return el[el.length - 1]
+}
+
+const getFirst = (input: string) => {
+  const el = input.split(/[\s.]+/)
+  return el[0]
+}
+
+// TODO : multilevel
+const getChildChart = (data: any, res: any): void => {
+  const nodes: object[] = []
+  if (typeof data === 'object') {
+    for (const key in data) {
+      if (getFirst(key) === 'source') {
+        res['name'] = getLast(key)
+        if (Array.isArray(data[key])) {
+          data[key].forEach((el) => {
+            const children = res['children'] || []
+            children.push({ name: getLast(el) })
+            res['children'] = children
+          })
+        } else {
+          nodes.push(data)
+        }
+      }
+    }
+  }
+}
 
 const Lineage = ({ url, dataset_id }) => {
   const [data, loading, error] = useLineage(url, dataset_id)
+
+  const children: object = {}
+  if (data) {
+    getChildChart(data['child_map'], children)
+  }
+
+  const spec_children = { option: getOption(children) }
 
   return (
     <React.Fragment>
       {data && (
         <Block>
-          <Label>Lineage</Label>
-          <div>TODO: draw graphs:</div>
-          {JSON.stringify(data['parent_map'])}
-          {JSON.stringify(data['child_map'])}
+          <Label>Kilde</Label>
+          <EChart spec={spec_children} height={600} />
         </Block>
       )}
     </React.Fragment>
