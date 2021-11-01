6aa861b62b0d4c9d74b6d028846cd37831f66b21
Kyrre Havik <kyrre.havik@nav.no>
2021-11-01 10:26:52 +0100

    fix: text to path logo

--- packages/shared/ui/src/components/header/logo.js
+++ packages/shared/ui/src/components/header/logo.js
@@ -27,17 +27,78 @@ export const logo = (
 )
 
 export const DataKatalogLogo = (
-  <svg
-    viewBox="0 0 158 40"
-    height="40"
-    width="158"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
+<svg
+   width="158"
+   height="40"
+   viewBox="0 0 158 40"
+   fill="none"
+   xmlns="http://www.w3.org/2000/svg"
   >
-    <rect x="150" width="8" height="8" fill="#5AC4FF" />
-    <path d="M134 8H0V40H142V16H134V8Z" fill="#5ac4ff" clipRule="evenodd" fillRule="evenodd" />
-    <rect x="142" y="8" width="8" height="8" fill="#0085CF" />
-    <text x="16.647169" y="29.999973" fill="#181818" font-family="Futura" font-size="14.976px" stroke-width=".93602"><tspan x="16.647169" y="29.999973" font-family="Futura" stroke-width=".93602">DATAPAKKER</tspan></text>
+  <rect
+     x="150"
+     width="8"
+     height="8"
+     fill="#5AC4FF"
+     id="rect833" />
+  <path
+     fillRule="evenodd"
+     clipRule="evenodd"
+     d="M134 8H0V40H142V16H134V8Z"
+     fill="#5AC4FF"
+     id="path835"
+     style="fill:#5ac4ff;fill-opacity:1" />
+  <rect
+     x="142"
+     y="8"
+     width="8"
+     height="8"
+     fill="#0085CF"
+     id="rect837" />
+  <g
+     style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:14.9762px;line-height:1.25;font-family:Futura;-inkscape-font-specification:Futura;fill:#181818;fill-opacity:1;stroke-width:0.936015"
+     id="text875"
+     aria-label="DATAPAKKER">
+    <path
+       id="path842"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="M 18,29.999973 V 18.709322 h 2.369281 q 1.703836,0 2.691036,0.33638 1.060327,0.329067 1.923213,1.118827 1.747711,1.594147 1.747711,4.190119 0,2.603285 -1.820836,4.212056 -0.914075,0.804386 -1.915901,1.118828 -0.936013,0.314441 -2.654473,0.314441 z m 1.703835,-1.601459 h 0.767823 q 1.148078,0 1.908588,-0.241315 0.76051,-0.255941 1.374768,-0.811699 1.257767,-1.148078 1.257767,-2.990852 0,-1.8574 -1.243142,-3.01279 -1.118827,-1.031077 -3.297981,-1.031077 h -0.767823 z" />
+    <path
+       id="path844"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 34.972539,27.257749 h -4.833627 l -1.257767,2.742224 H 27.045683 L 32.632508,18 38.021893,29.999973 H 36.15718 Z M 34.277843,25.65629 32.603258,21.817176 30.848234,25.65629 Z" />
+    <path
+       id="path846"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 41.926819,20.310781 v 9.689192 h -1.703835 v -9.689192 h -2.595972 v -1.601459 h 6.888467 v 1.601459 z" />
+    <path
+       id="path848"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 52.054769,27.257749 h -4.833627 l -1.257767,2.742224 H 44.127913 L 49.714738,18 55.104122,29.999973 H 53.23941 Z M 51.360072,25.65629 49.685487,21.817176 47.930464,25.65629 Z" />
+    <path
+       id="path850"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 58.226601,25.414975 v 4.584998 H 56.522766 V 18.709322 h 1.930526 q 1.418644,0 2.142591,0.197441 0.731259,0.19744 1.287017,0.745885 0.972575,0.950637 0.972575,2.398532 0,1.55027 -1.038388,2.457032 -1.038389,0.906763 -2.800725,0.906763 z m 0,-1.579522 h 0.636196 q 2.347344,0 2.347344,-1.806211 0,-1.747711 -2.42047,-1.747711 h -0.56307 z" />
+    <path
+       id="path852"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 70.43864,27.257749 h -4.833627 l -1.257767,2.742224 H 62.511784 L 68.098609,18 73.487993,29.999973 H 71.623281 Z M 69.743943,25.65629 68.069358,21.817176 66.314335,25.65629 Z" />
+    <path
+       id="path854"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 76.595845,23.213883 4.394871,-4.504561 h 2.274218 l -5.104193,5.111506 5.133443,6.179145 h -2.28153 l -4.065805,-5.023754 -0.351004,0.351004 v 4.67275 H 74.892009 V 18.709322 h 1.703836 z" />
+    <path
+       id="path856"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 86.511727,23.213883 4.394871,-4.504561 h 2.274218 l -5.104193,5.111506 5.133443,6.179145 h -2.28153 l -4.065805,-5.023754 -0.351004,0.351004 v 4.67275 H 84.807892 V 18.709322 h 1.703835 z" />
+    <path
+       id="path858"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 100.95411,20.310781 h -4.526497 v 2.712974 h 4.394867 v 1.601459 h -4.394867 v 3.7733 h 4.526497 v 1.601459 H 94.723777 V 18.709322 h 6.230333 z" />
+    <path
+       id="path860"
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:Futura;-inkscape-font-specification:Futura;stroke-width:0.936015"
+       d="m 107.03088,25.188284 3.49542,4.811689 h -2.08409 l -3.22486,-4.621561 h -0.30712 v 4.621561 h -1.70384 V 18.709322 h 1.99634 q 2.23765,0 3.23217,0.840949 1.09689,0.936013 1.09689,2.471658 0,1.199266 -0.68739,2.062153 -0.68738,0.862886 -1.81352,1.104202 z m -2.12065,-1.29433 h 0.54113 q 2.42047,0 2.42047,-1.850087 0,-1.733086 -2.35466,-1.733086 h -0.60694 z" />
+  </g>
+</svg>
 
-  </svg>
 )
